af61722a6e561bb8251d7ac6975b50e598aa4914
Stefano Rodighiero <larsen@marvin>
2015-11-10 11:58:48 +0100

    some layout tweaks; some key combos

--- xmonad.hs
+++ xmonad.hs
@@ -3,18 +3,27 @@ import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
+import XMonad.Layout.Spacing
+import XMonad.Layout.NoBorders
 import System.IO
+import qualified Data.Map as M
 
 main = do
   xmproc <- spawnPipe "xmobar"
 
   xmonad $ defaultConfig 
     { manageHook = manageDocks <+> manageHook defaultConfig
-    , layoutHook = avoidStruts  $  layoutHook defaultConfig
+    -- , layoutHook = avoidStruts  $ layoutHook defaultConfig
+    , layoutHook = avoidStruts $ spacing 2$ smartBorders $ layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle = xmobarColor "grey" "" . shorten 50
       }
     , borderWidth = 1
     , normalBorderColor = "#cccccc"
-    , focusedBorderColor = "#ccbb00" }
+    , focusedBorderColor = "#ccbb00"
+    , modMask = mod1Mask
+    } `additionalKeys`
+    [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+    , ((mod1Mask .|. shiftMask, xK_e), spawn "emacsclient -c")
+    ]
