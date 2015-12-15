dc5ba7e26419a265e876ee8524d1be5f18e59e81
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2015-12-15 14:07:44 +0100

    scratchpad test

--- xmonad.hs
+++ xmonad.hs
@@ -5,16 +5,28 @@ import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
+import XMonad.Util.Scratchpad (scratchpadManageHook, scratchpadSpawnActionTerminal)
 import System.IO
 import qualified Data.Map as M
+import qualified XMonad.StackSet as W
+
+manageScratchpad :: ManageHook
+manageScratchpad = scratchpadManageHook (W.RationalRect l t w h)
+  where h = 0.1
+        w = 0.1
+        t = 0.1 
+        l = 0.1
+
+myManageHook = manageDocks
+  <+> manageScratchpad
+  <+> manageHook defaultConfig
 
 main = do
   xmproc <- spawnPipe "xmobar"
 
   xmonad $ defaultConfig 
-    { manageHook = manageDocks <+> manageHook defaultConfig
-    -- , layoutHook = avoidStruts  $ layoutHook defaultConfig
-    , layoutHook = avoidStruts $ spacing 2$ smartBorders $ layoutHook defaultConfig
+    { manageHook = myManageHook
+    , layoutHook = avoidStruts $ spacing 2 $ smartBorders $ layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle = xmobarColor "grey" "" . shorten 50
@@ -26,4 +38,5 @@ main = do
     } `additionalKeys`
     [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
     , ((mod1Mask .|. shiftMask, xK_e), spawn "emacsclient -c")
+    , ((mod1Mask .|. shiftMask, xK_s), scratchpadSpawnActionTerminal "xclock")
     ]
