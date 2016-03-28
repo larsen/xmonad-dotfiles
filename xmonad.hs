bfecc17445f43f49b208c3690a15372e50819092
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-03-28 19:13:03 +0200

    lastest changes

--- xmonad.hs
+++ xmonad.hs
@@ -1,6 +1,10 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.UrgencyHook
 import XMonad.Hooks.ManageDocks
+import XMonad.Actions.CycleWS
+import XMonad.Actions.GridSelect
+import XMonad.Actions.WindowBringer
 import XMonad.Util.Run (spawnPipe)
 import XMonad.Util.EZConfig (additionalKeys)
 import XMonad.Layout.Spacing
@@ -17,26 +21,47 @@ manageScratchpad = scratchpadManageHook (W.RationalRect l t w h)
         t = 0.1 
         l = 0.1
 
+myUrgencyHook = dzenUrgencyHook {
+  args = ["-bg", "darkred", "-fg", "grey80", "-xs", "1"] }
+
 myManageHook = manageDocks
   <+> manageScratchpad
   <+> manageHook defaultConfig
 
+myTerminal = "urxvt -bg black"
+-- myTerminal = "mate-terminal"
+myFont = "Droid Sans Mono-16"
+
+myGSConfig = defaultGSConfig { gs_cellwidth = 250 }
+
 main = do
   xmproc <- spawnPipe "xmobar"
 
-  xmonad $ defaultConfig 
+  xmonad $ withUrgencyHook myUrgencyHook $ defaultConfig 
     { manageHook = myManageHook
     , layoutHook = avoidStruts $ spacing 2 $ smartBorders $ layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle = xmobarColor "grey" "" . shorten 50
       }
+    , terminal = myTerminal
     , borderWidth = 1
     , normalBorderColor = "#cccccc"
-    , focusedBorderColor = "#ccbb00"
-    , modMask = mod1Mask
+    , focusedBorderColor = "cadetblue"
+    -- , focusedBorderColor = "#ff0000"
+    , modMask = mod4Mask
     } `additionalKeys`
-    [ ((mod1Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-    , ((mod1Mask .|. shiftMask, xK_e), spawn "emacsclient -c")
-    , ((mod1Mask .|. shiftMask, xK_s), scratchpadSpawnActionTerminal "xclock")
+    [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
+    , ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient -c")
+    , ((mod4Mask .|. shiftMask, xK_s), spawn "mate-screenshot -i")
+    , ((mod4Mask,               xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-16'")
+      -- CycleWS
+    , ((mod4Mask,               xK_Right),  nextWS)
+    , ((mod4Mask,               xK_Left),    prevWS)
+      -- GridSelect
+    , ((mod4Mask,               xK_g ), goToSelected myGSConfig)
+      -- WindowBringer
+    , ((mod4Mask,               xK_b ), gotoMenuArgs ["-fn", myFont])
+    , ((mod4Mask .|. shiftMask, xK_b ), bringMenuArgs ["-fn", myFont])      
+    -- , ((mod4Mask .|. shiftMask, xK_s), scratchpadSpawnActionTerminal "")
     ]
