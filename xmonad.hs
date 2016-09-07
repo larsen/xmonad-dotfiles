934c5af30bc56b2e2738817a17b1344f33add172
Stefano Rodighiero <stefano.rodighiero@gmail.com>
2016-09-07 16:28:06 +0200

    simplified key bindings configuration

--- xmonad.hs
+++ xmonad.hs
@@ -7,7 +7,7 @@ import XMonad.Actions.GridSelect
 import XMonad.Actions.WindowBringer
 import qualified XMonad.Actions.DynamicWorkspaceOrder as DO
 import XMonad.Util.Run (spawnPipe)
-import XMonad.Util.EZConfig (additionalKeysP, additionalKeys)
+import XMonad.Util.EZConfig (additionalKeysP)
 import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Util.Scratchpad (scratchpadManageHook, scratchpadSpawnActionTerminal)
@@ -29,7 +29,6 @@ myManageHook = manageDocks
   <+> manageHook defaultConfig
 
 myTerminal = "urxvt -bg black"
--- myTerminal = "mate-terminal"
 myFont = "Inconsolata-16"
 
 myGSConfig = defaultGSConfig { gs_cellwidth = 250 }
@@ -39,7 +38,7 @@ main = do
 
   xmonad $ withUrgencyHook myUrgencyHook $ defaultConfig 
     { manageHook = myManageHook
-    , layoutHook = avoidStruts $ spacing 2 $ smartBorders $ layoutHook defaultConfig
+    , layoutHook = avoidStruts $ spacing 0 $ smartBorders $ layoutHook defaultConfig
     , logHook = dynamicLogWithPP xmobarPP
       { ppOutput = hPutStrLn xmproc
       , ppTitle = xmobarColor "grey" "" . shorten 25
@@ -56,36 +55,30 @@ main = do
     , terminal = myTerminal
     , borderWidth = 1
     , normalBorderColor = "#cccccc"
-    , focusedBorderColor = "cadetblue"
+    , focusedBorderColor = "yellow"
     -- , focusedBorderColor = "#ff0000"
     , modMask = mod4Mask
-    } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_z), spawn "xscreensaver-command -lock")
-    , ((mod4Mask .|. shiftMask, xK_e), spawn "emacsclient -c")
-    , ((mod4Mask .|. shiftMask, xK_s), spawn "scrot -e 'mv $f ~/Pictures/screenshots/'")
-    , ((mod4Mask,               xK_p), spawn "dmenu_run -fn 'Droid Sans Mono-16'")
-      -- 
-    , ((mod4Mask,               xK_Right), DO.moveTo Next HiddenNonEmptyWS)
-    , ((mod4Mask,               xK_Left),  DO.moveTo Prev HiddenNonEmptyWS)
-      -- GridSelect
-    , ((mod4Mask,               xK_g ), goToSelected myGSConfig)
-      -- WindowBringer
-    , ((mod4Mask,               xK_b ), gotoMenuArgs ["-fn", myFont])
-    , ((mod4Mask .|. shiftMask, xK_b ), bringMenuArgs ["-fn", myFont])      
-
-    , ((mod4Mask .|. controlMask,  xK_Right), DO.swapWith Next NonEmptyWS)
-    , ((mod4Mask .|. controlMask,  xK_Left), DO.swapWith Prev NonEmptyWS)
-
-    -- , ("M-S-<R>",   DO.shiftTo Next HiddenNonEmptyWS)
-    -- , ("M-S-<L>",   DO.shiftTo Prev HiddenNonEmptyWS)
-    -- , ("M-<R>",     DO.moveTo Next HiddenNonEmptyWS)
-    -- , ("M-<L>",     DO.moveTo Prev HiddenNonEmptyWS)
-
-    -- , ((mod4Mask .|. shiftMask, xK_s), scratchpadSpawnActionTerminal "")
-    ]
+    }
     `additionalKeysP`
-      map (\ (key, event) -> (key, spawn $ "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player." ++ event))
+      ( [ ("M-S-z", spawn "xscreensaver-command -lock")
+        , ("M-S-e", spawn "emacsclient -c")
+        , ("M-S-s", spawn "scrot -e 'mv $f ~/Pictures/screenshots/'")
+        , ("M-S-t", spawn "/usr/bin/curl --user tiro:JF4NabjScClXkgx 192.168.20.9/tiro.py")
+        , ("M-p",   spawn "dmenu_run -fn 'Droid Sans Mono-16'")
+        , ("M-<R>", DO.moveTo Next HiddenNonEmptyWS)
+        , ("M-<L>", DO.moveTo Prev HiddenNonEmptyWS)
+
+        -- GridSelect
+        , ("M-g",   goToSelected myGSConfig)
+        -- WindowBringer
+        , ("M-b",     gotoMenuArgs ["-fn", myFont])
+        , ("M-S-b",   bringMenuArgs ["-fn", myFont])
+        , ("M-C-<R>", DO.swapWith Next NonEmptyWS)
+        , ("M-C-<L>", DO.swapWith Prev NonEmptyWS)
+        ] 
+      ++ 
+        map (\ (key, event) -> (key, spawn $ "dbus-send --print-reply --dest=org.mpris.MediaPlayer2.spotify /org/mpris/MediaPlayer2 org.mpris.MediaPlayer2.Player." ++ event))
         [ ("<XF86AudioPlay>", "PlayPause")
         , ("<XF86AudioStop>", "Stop")
         , ("<XF86AudioPrev>", "Previous")
-        , ("<XF86AudioNext>", "Next") ]
+        , ("<XF86AudioNext>", "Next") ] )
